6762f5625d0cc957e44ea78d015048d60ebbb7c2
Lee Jun <leejun.nakao@gmail.com>
2025-09-12 13:14:00 +0000

    fix type imports in index.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import type { PluginInit } from "passauth/plugin/interfaces";
-import type { EmailPluginOptions, UserEmailSenderPlugin } from "./interfaces";
+import type { EmailPluginOptions, UserEmailSenderPlugin } from "./interfaces/index.d";
 export declare const EmailSenderPlugin: PluginInit<UserEmailSenderPlugin, EmailPluginOptions>;
 //# sourceMappingURL=index.d.ts.map
