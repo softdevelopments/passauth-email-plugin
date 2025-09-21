4fda2f97931cb406d677219c15eab46f6b653ec6
Lee Jun <leejun.nakao@gmail.com>
2025-09-21 15:10:04 +0000

    feat: use passauth^1.0.12

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,10 @@
 import { AuthHandler } from "passauth/auth";
+import {
+  PassauthInvalidCredentialsException,
+  PassauthInvalidUserException,
+} from "passauth/auth/exceptions";
 import type { LoginParams, RegisterParams } from "passauth/auth/interfaces";
+import { compareHash } from "passauth/auth/utils";
 import { generateToken } from "../utils";
 import {
   DEFAULT_CONFIRMATION_LINK_EXPIRATION_MS,
@@ -15,11 +20,6 @@ import {
   type SendEmailArgs,
   type UserPluginEmailSender,
 } from "../interfaces/types";
-import {
-  PassauthInvalidCredentialsException,
-  PassauthInvalidUserException,
-} from "passauth/auth/exceptions";
-import { compareHash } from "passauth/auth/utils";
 import { TemplateTypes } from "../interfaces/enum";
 
 export class EmailSender {
@@ -44,7 +44,7 @@ export class EmailSender {
 
   constructor(
     private options: EmailPluginOptions,
-    private authHandler: AuthHandler<UserPluginEmailSender>,
+    private authHandler: AuthHandler<UserPluginEmailSender>
   ) {
     const confirmationExpiration =
       options.emailConfig?.[TemplateTypes.CONFIRM_EMAIL]?.linkExpirationMs;
@@ -85,7 +85,7 @@ export class EmailSender {
           text,
           html,
         },
-        TemplateTypes.CONFIRM_EMAIL,
+        TemplateTypes.CONFIRM_EMAIL
       );
 
       await this.options.client.send(params);
@@ -101,7 +101,7 @@ export class EmailSender {
       const isValid = this.verifyToken(
         email,
         token,
-        TemplateTypes.CONFIRM_EMAIL,
+        TemplateTypes.CONFIRM_EMAIL
       );
 
       if (isValid) {
@@ -133,7 +133,7 @@ export class EmailSender {
           text,
           html,
         },
-        TemplateTypes.RESET_PASSWORD,
+        TemplateTypes.RESET_PASSWORD
       );
 
       await this.options.client.send(params);
@@ -149,7 +149,7 @@ export class EmailSender {
       const isValid = this.verifyToken(
         email,
         token,
-        TemplateTypes.RESET_PASSWORD,
+        TemplateTypes.RESET_PASSWORD
       );
 
       if (isValid) {
@@ -168,7 +168,7 @@ export class EmailSender {
 
   async login<T extends UserPluginEmailSender>(
     params: LoginParams,
-    jwtUserFields?: Array<keyof T>,
+    jwtUserFields?: Array<keyof T>
   ) {
     const user = (await this.authHandler.repo.getUser({
       email: params.email,
@@ -265,7 +265,7 @@ export class EmailSender {
 
   private getEmailParams(
     emailArgs: Pick<SendEmailArgs, "to" | "subject" | "text" | "html">,
-    templateType: TemplateTypes,
+    templateType: TemplateTypes
   ): SendEmailArgs {
     const overrideParams = this.options.emailConfig?.[templateType]?.email;
 
@@ -291,7 +291,7 @@ export class EmailSender {
 
 export const EmailPlugin = (
   options: EmailPluginOptions,
-  authHandler: AuthHandler<UserPluginEmailSender>,
+  authHandler: AuthHandler<UserPluginEmailSender>
 ) => {
   if (!options.senderName) {
     throw new PassauthEmailPluginMissingConfigurationException("senderName");
